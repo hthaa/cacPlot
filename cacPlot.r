28b94a19400714d71f6b8f53131bfe5a714b8c13
hthaa <53649487+hthaa@users.noreply.github.com>
2019-08-13 10:56:33 +0200

    Update cacPlot.r

--- cacPlot.r
+++ cacPlot.r
@@ -26,10 +26,16 @@ cacGradient <- function(x, colorblindFriendly = FALSE) {
   )
 }
 
-cacPlot <- function(x, ablty = NULL, ablty.se = NULL, stat = "cc", mdl = "Rasch", cutoff = 0, cSEM = TRUE, xRng = c(-3, 3), yRng = c(0, 1), grid = TRUE, lbls = TRUE, lgd = TRUE, grp = NULL, rel.wdth = c(7, 1), colorblindFriendly = FALSE) {
+cacPlot <- function(x, ablty = NULL, ablty.se = NULL, stat = "cc", mdl = "Rasch", cutoff = 0, ci = TRUE, cSEM = TRUE, xRng = c(-3, 3), yRng = c(0, 1), grid = TRUE, lbls = TRUE, lgd = TRUE, grp = NULL, rel.wdth = c(7, 1), colorblindFriendly = FALSE) {
   library(mirt)
   library(cacIRT)
   if (!is.null(ablty) & is.null(ablty.se)) stop("Raw ability estimates must be accompanied by standard errors to compute classification accuracy or consistency.")
+  if (stat == "cc" | stat == "c" | stat == "consistency") { 
+    stat <- "Consistency" 
+  }
+  if (stat == "ca" | stat == "a" | stat == "accuracy") { 
+    stat <- "Accuracy" 
+  }
   layout(matrix(c(1, 2), ncol = 2), c(rel.wdth[1], rel.wdth[2]), c(1, 1))
   par(mar = c(4, 3, 3, 1))
   if (class(x) == "SingleGroupClass") {
@@ -50,46 +56,26 @@ cacPlot <- function(x, ablty = NULL, ablty.se = NULL, stat = "cc", mdl = "Rasch"
   }
   abline(v = cutoff, lty = 3, lwd = 2)
   curve(dnorm(x), from = xRng[1], to = xRng[2], lwd = 2, n = 1001, add = TRUE)
-  for (i in 1:nrow(ab.est)) {
-    if (stat == "cc" | stat == "c" | stat == "consistency") { 
-      stat <- "Consistency" 
-    }
-    if (stat == "ca" | stat == "a" | stat == "accuracy") { 
-      stat <- "Accuracy" 
-    }
-      col <- rgb(
-        red = if (cac$Conditional[[stat]][i] <= .75) {
-          1
-        } else {
-          (1 - cac$Conditional[[stat]][i]) * 4
-        },
-        green = if (!colorblindFriendly) {
-          if (cac$Conditional[[stat]][i] >= .75) {
-            1
-          } else {
-            (cac$Conditional[[stat]][i] - .5) * 4
-          }
-        } else {
-          0
-        },
-        blue = if (colorblindFriendly) {
-          if (cac$Conditional[[stat]][i] >= .75) {
-            1
-          } else {
-            (cac$Conditional[[stat]][i] - .5) * 4
-          }
-        } else {
-          0
-        }
-      )
-    
-    # Point estimate of Theta.
-    points(ab.est[i, 1], dnorm(ab.est[i, 1]), pch = 19, col = col)
-    # 95% CI.
-    lines(c(ab.est[i, 1] - 1.96*ab.est[i, 2], ab.est[i, 1] + 1.96*ab.est[i, 2]), rep(dnorm(ab.est[i, 1]), 2), lwd = 2, col = col)
-    lines(rep(ab.est[i, 1] - 1.96*ab.est[i, 2], 2), c(dnorm(ab.est[i, 1] - (yRng[2] - yRng[1]) * .03), dnorm(ab.est[i, 1] + (yRng[2] - yRng[1]) * .03)), lwd = 2, col = col)
-    lines(rep(ab.est[i, 1] + 1.96*ab.est[i, 2], 2), c(dnorm(ab.est[i, 1] - (yRng[2] - yRng[1]) * .03), dnorm(ab.est[i, 1] + (yRng[2] - yRng[1]) * .03)), lwd = 2, col = col)
+
+  cac.col <- sapply(cac$Conditional[[stat]], cacGradient)
+  # Point estimate of Theta.
+  points(matrix(c(ab.est[, 1], sapply(ab.est[, 1], dnorm)), ncol = 2), pch = 19, col = cac.col)
+  
+  if (ci) {
+    coords <- matrix(c(ab.est[, 1] - 1.96*ab.est[, 2], ab.est[, 1] + 1.96*ab.est[, 2], sapply(ab.est[, 1], dnorm)), ncol = 3)
+    apply(coords, 1, function(x) {
+      lines(x[-3], rep(x[3], 2), col = cacGradient(class.Rud(cutoff, ability = ((x[1] + x[2]) / 2), se = x[3])$Conditional[[stat]]), lwd = 2)
+      lines(rep(x[1], 2), c(x[3] - (yRng[2] - yRng[1]) * .015, x[3] + (yRng[2] - yRng[1]) * .015), col = cacGradient(class.Rud(cutoff, ability = ((x[1] + x[2]) / 2), se = x[3])$Conditional[[stat]]), lwd = 2)
+      lines(rep(x[2], 2), c(x[3] - (yRng[2] - yRng[1]) * .015, x[3] + (yRng[2] - yRng[1]) * .015), col = cacGradient(class.Rud(cutoff, ability = ((x[1] + x[2]) / 2), se = x[3])$Conditional[[stat]]), lwd = 2)
+      }
+    )
   }
+  
+  #lines(matrix(c(ab.est[, 1] - 1.96*ab.est[, 2], ab.est[, 1] + 1.96*ab.est[, 2]), ncol = 2), rep(sapply(ab.est[, 1], dnorm), 2), lwd = 2, col = cac.col)
+  #lines(rep(ab.est[, 1] - 1.96*ab.est[, 2], 2), 
+  #      c(dnorm(ab.est[, 1] - (yRng[2] - yRng[1]) * .03), dnorm(ab.est[, 1] + (yRng[2] - yRng[1]) * .03)), lwd = 2, col = cac.col)
+  #lines(rep(ab.est[, 1] + 1.96*ab.est[, 2], 2), 
+  #      c(dnorm(ab.est[, 1] - (yRng[2] - yRng[1]) * .03), dnorm(ab.est[, 1] + (yRng[2] - yRng[1]) * .03)), lwd = 2, col = cac.col)
   if (lgd) {
     legend("topleft", bty = "n", lty = c(1, 2, 3), lwd = c(2, 2, 2), pch = c(19, NA_integer_, NA_integer_), legend = c("Obs. w/ 95% CI", "cSEM", paste("Cutoff (", cutoff, ")", sep = "")), merge = TRUE) 
   }
