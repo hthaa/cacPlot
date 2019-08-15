7416b91be9559e8d42d6ecd0f490bc3d9aeb7eef
Haakon Thorbergsen Haakstad <thorbergsen@gmail.com>
2019-08-15 15:55:55 +0200

    Typo in description fix

--- R/cacPlot.R
+++ R/cacPlot.R
@@ -3,8 +3,8 @@
 #' @description Plotting tool for gauging the classification accuracy or consistency of classifications based on IRT ability estimates, where classification accuracy and consistency indices are computed by means of the \code{cacIRT} package (Lathrop, 2015).
 #'
 #' @param x A data.frame or matrix with rows representing respondents and columns representing items, or a mirt-model object of class "SingleGroupClass".
-#' @param ablty A vector of ability estimates. Required specification of standard error in \code{ablty.se}.
-#' @param ablty.se A vector of standard errors of estimates corresponding to the values in the \code{ablty} vector.
+#' @param ablty A vector of ability estimates. Requires specification of standard error in \code{ablty.se}.
+#' @param ablty.se A vector of standard errors of estimates corresponding to the values in the \code{ablty} vector. Must be supplied if raw ability estimates are to be used for plotting.
 #' @param cutoff The cutoff value relative to which expected classification consistency or accuracy for observations are to be calculated and illustrated.
 #' @param stat A character-value indicating whether to color-code observations with respect to their expected consistency or accuracy. Permissible values are "c", "cc", "consistency" or "Consistency" for expected classification consistency, and "a", "ca", "accuracy" or "Accuracy" for expected classification accuracy.
 #' @param ci Plot confidence intervals around each observation point?
