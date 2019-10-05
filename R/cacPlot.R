1da09c4e9538198542bac41352a19795b061f074
Haakon Thorbergsen Haakstad <thorbergsen@gmail.com>
2019-10-05 21:58:15 +0200

    Added possibility of plotting several cutoff points.

--- R/cacPlot.R
+++ R/cacPlot.R
@@ -7,6 +7,7 @@
 #' @param x A data.frame or matrix with rows representing respondents and columns representing items, or a mirt-model object of class "SingleGroupClass".
 #' @param ablty A vector of ability estimates. Requires specification of standard error in \code{ablty.se}.
 #' @param ablty.se A vector of standard errors of estimates corresponding to the values in the \code{ablty} vector. Must be supplied if raw ability estimates are to be used for plotting.
+#' @param pop.dist Population distribution. Specifies the mean and standard deviation of the population distribution along which individual Theta values are plotted.
 #' @param cutoff The cutoff value relative to which expected classification consistency or accuracy for observations are to be calculated and illustrated.
 #' @param stat A character-value indicating whether to color-code observations with respect to their expected consistency or accuracy. Permissible values are "c", "cc", "consistency" or "Consistency" for expected classification consistency, and "a", "ca", "accuracy" or "Accuracy" for expected classification accuracy.
 #' @param ci Plot confidence intervals around each observation point?
@@ -22,7 +23,7 @@
 #' @return A graph plotting observations with color gradients indicating expected classification consistency and accuracy relative to a defined cutoff point.
 #' @references R. Philip Chalmers (2012). mirt: A Multidimensional Item Response Theory Package for the R Environment. Journal of Statistical Software, 48(6), 1-29.
 #' @references Quinn N. Lathrop (2015). cacIRT: Classification Accuracy and Consistency under Item Response Theory. R package version 1.4.
-#' @references Lawrence M. Rudner (2001). Computing the Expected PRoportions of Misclassified Examinees. Practical Assessment, Research & Evaluation., 7(14), 1-6.
+#' @references Lawrence M. Rudner (2001). Computing the Expected Proportions of Misclassified Examinees. Practical Assessment, Research & Evaluation., 7(14), 1-6.
 #' @references Lawrence M. Rudner (2005). Expected Classification Accuracy. Practical Assessment, Research & Evaluation, 10(13), 1-5.
 #' @examples
 #' # Color-blind friendly plotting of classification consistency based on feeding
@@ -44,9 +45,14 @@
 #' ability_estimates <- fscores(mirt(expand.table(LSAT7[2:31, ]), 1, "Rasch"),
 #'     "ML", response.pattern = expand.table(LSAT7[2:31, ]))[, c("F1", "SE_F1")]
 #' cacPlot(ablty = ability_estimates[, "F1"], ablty.se = ability_estimates[, "SE_F1"], stat = "c")
+#'
+#' # Plotting of classification accuracy with several cutoff points
+#' data <- expand.table(LSAT7[2:31, ])
+#' LSAT7.mod <- mirt(data, model = 1, itemtype = "Rasch")
+#' cacPlot(LSAT7.mod, stat = "a", cutoff = c(-.5, 0, .5))
 #' @export
 
-cacPlot <- function(x = NULL, ablty = NULL, ablty.se = NULL, cutoff = 0, stat = "ca", ci = TRUE, cSEM = FALSE, xRng = c(-3, 3), yRng = c(0, .5), grid = TRUE, lbls = TRUE, lgd = TRUE, rel.wdth = c(7, 1), mdl = "Rasch", ablty.est = "ML", colorblindFriendly = FALSE) {
+cacPlot <- function(x = NULL, ablty = NULL, ablty.se = NULL, pop.dist = c(0, 1), cutoff = 0, stat = "ca", ci = TRUE, cSEM = FALSE, xRng = c(-3, 3), yRng = c(0, .5), grid = TRUE, lbls = TRUE, rel.wdth = c(7, 1), mdl = "Rasch", ablty.est = "ML", colorblindFriendly = FALSE) {
   if (!is.null(ablty) & is.null(ablty.se)) {
     stop("Raw ability estimates must be accompanied by standard errors to compute classification accuracy or consistency.")
   }
@@ -75,6 +81,11 @@ cacPlot <- function(x = NULL, ablty = NULL, ablty.se = NULL, cutoff = 0, stat = 
     matrix(c(ablty, ablty.se), ncol = 2)
   }
   cac <- cacIRT::class.Rud(cutoff, ability = ab.est[, 1], se = ab.est[, 2], D = 1)
+  if (length(cutoff) > 1) {
+    cac <- cac$Conditional[[stat]][, -ncol(cac$Conditional[[stat]])]
+    } else {
+      cac <- cac$Conditional[[stat]]
+    }
   plot(NULL, xlim = xRng, ylim = yRng, xlab = "", ylab = "")
   if (grid) grid()
   if (cSEM) {
@@ -82,28 +93,34 @@ cacPlot <- function(x = NULL, ablty = NULL, ablty.se = NULL, cutoff = 0, stat = 
     plot(seq(xRng[1], xRng[2], .001), 1 / sqrt(mirt::testinfo(mod, seq(xRng[1], xRng[2], .001))),
          xlim = xRng, ylim = yRng, type = "l", lty = 2, lwd = 2, xlab = "", ylab = "")
   }
-  abline(v = cutoff, lty = 3, lwd = 2)
-  curve(dnorm(x), from = xRng[1], to = xRng[2], lwd = 2, n = 1001, add = TRUE)
-  cac.col <- sapply(cac$Conditional[[stat]], cacGradient, cp = colorblindFriendly)
-  points(matrix(c(ab.est[, 1], sapply(ab.est[, 1], dnorm)), ncol = 2), pch = 19, col = cac.col)
+  sapply(cutoff, function(x) abline(v = x, lty = 3, lwd = 2))
+  curve(dnorm(x, pop.dist[1], pop.dist[2]), from = xRng[1], to = xRng[2], lwd = 2, n = 1001, add = TRUE)
+  cac.col <- apply(cac, 1, function(x) { cacGradient(if (length(cutoff > 1)) { min(x) } else { x }, cp = colorblindFriendly) })
+
+  points(matrix(c(ab.est[, 1], sapply(ab.est[, 1], dnorm, mean = pop.dist[1], pop.dist[2])), ncol = 2), pch = 19, col = cac.col)
   if (ci) {
-    coords <- matrix(c(ab.est[, 1], ab.est[, 2], ab.est[, 1] - 1.96*ab.est[, 2], ab.est[, 1] + 1.96*ab.est[, 2], sapply(ab.est[, 1], dnorm)), ncol = 5)
+    coords <- matrix(c(ab.est[, 1], ab.est[, 2], ab.est[, 1] - 1.96*ab.est[, 2], ab.est[, 1] + 1.96*ab.est[, 2], sapply(ab.est[, 1], dnorm, mean = pop.dist[1], sd = pop.dist[2])), ncol = 5)
     apply(coords, 1, function(x) {
-      lines(x[c(3, 4)], rep(x[5], 2), col = cacGradient(cacIRT::class.Rud(cutoff, D = 1, ability = x[1], se = x[2])$Conditional[[stat]],
-                                                        colorblindFriendly), lwd = 2)
-      lines(rep(x[3], 2), c(x[5] - (yRng[2] - yRng[1]) * .015, x[5] + (yRng[2] - yRng[1]) * .015),
-            col = cacGradient(cacIRT::class.Rud(cutoff, D = 1, ability = x[1], se = x[2])$Conditional[[stat]], colorblindFriendly), lwd = 2)
-      lines(rep(x[4], 2), c(x[5] - (yRng[2] - yRng[1]) * .015, x[5] + (yRng[2] - yRng[1]) * .015),
-            col = cacGradient(cacIRT::class.Rud(cutoff, D = 1, ability = x[1], se = x[2])$Conditional[[stat]], colorblindFriendly), lwd = 2)
-    }
+      if (length(cutoff > 1)) {
+        lines(x[c(3, 4)], rep(x[5], 2), col = cacGradient(min(cacIRT::class.Rud(cutoff, D = 1, ability = x[1], se = x[2])$Conditional[[stat]][, -(length(cutoff) + 1)]),
+                                                          colorblindFriendly), lwd = 2)
+        lines(rep(x[3], 2), c(x[5] - (yRng[2] - yRng[1]) * .015, x[5] + (yRng[2] - yRng[1]) * .015),
+              col = cacGradient(min(cacIRT::class.Rud(cutoff, D = 1, ability = x[1], se = x[2])$Conditional[[stat]][, -(length(cutoff) + 1)]), colorblindFriendly), lwd = 2)
+        lines(rep(x[4], 2), c(x[5] - (yRng[2] - yRng[1]) * .015, x[5] + (yRng[2] - yRng[1]) * .015),
+              col = cacGradient(min(cacIRT::class.Rud(cutoff, D = 1, ability = x[1], se = x[2])$Conditional[[stat]][, -(length(cutoff) + 1)]), colorblindFriendly), lwd = 2)
+      } else {
+        lines(x[c(3, 4)], rep(x[5], 2), col = cacGradient(min(cacIRT::class.Rud(cutoff, D = 1, ability = x[1], se = x[2])$Conditional[[stat]]),
+                                                          colorblindFriendly), lwd = 2)
+        lines(rep(x[3], 2), c(x[5] - (yRng[2] - yRng[1]) * .015, x[5] + (yRng[2] - yRng[1]) * .015),
+              col = cacGradient(min(cacIRT::class.Rud(cutoff, D = 1, ability = x[1], se = x[2])$Conditional[[stat]]), colorblindFriendly), lwd = 2)
+        lines(rep(x[4], 2), c(x[5] - (yRng[2] - yRng[1]) * .015, x[5] + (yRng[2] - yRng[1]) * .015),
+              col = cacGradient(min(cacIRT::class.Rud(cutoff, D = 1, ability = x[1], se = x[2])$Conditional[[stat]]), colorblindFriendly), lwd = 2)
+      }
+     }
     )
   }
-  if (lgd) {
-    legend("topleft", bty = "n", lty = c(1, 2, 3), lwd = c(2, 2, 2), pch = c(19, NA_integer_, NA_integer_),
-           legend = c("Obs. w/ 95% CI", "cSEM", paste("Cutoff (", cutoff, ")", sep = "")), merge = TRUE)
-  }
   if (lbls) {
-    title(main = paste("Expected Classification ", stat, " at Cutoff = ", cutoff, ".", sep = ""))
+    title(main = paste("Expected Classification ", stat, ".", sep = ""))
     par(cex = 1.5)
     title(xlab = expression(Theta))
   }
