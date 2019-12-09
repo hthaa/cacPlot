44b849112af6d0f1a4f9dbb888a8be2d6bc0c955
Haakon Thorbergsen Haakstad <thorbergsen@gmail.com>
2019-12-09 12:22:16 +0100

    fix specificity

--- R/cmat.R
+++ R/cmat.R
@@ -21,13 +21,13 @@ cmat <- function(score, se, cut) {
 #'
 #' @description Given a confusion matrix of the sort \code{matrix(c(tp, fp, tn, fn), nrow = 2)}, calculate common diagnostic performance statistics.
 #' @param cmat A confusion matrix of true and false positives and negatives, organized as \code{matrix(c(tp, fp, tn, fn), nrow = 2)}.
-#' @returns A list of diagnostic performance statistics, namely the Sensitivity, Specificity, Positive and Negative Likelihood Ratios (LR+ and LR-), Positive and Negative Predictive values (PPV and NPV), and the Youden's J statistic.
+#' @return A list of diagnostic performance statistics, namely the Sensitivity, Specificity, Positive and Negative Likelihood Ratios (LR+ and LR-), Positive and Negative Predictive values (PPV and NPV), and the Youden's J statistic.
 #' @export
 
 dpstats <- function(cmat = NULL) {
     dp <- list()
     dp[["Sensitivity"]] <- cmat[1, 1] / (cmat[2, 2] + cmat[1, 1])
-    dp[["Specificity"]] <- cmat[2, 1] / (cmat[2, 1] + cmat[1, 2])
+    dp[["Specificity"]] <- cmat[1, 2] / (cmat[2, 1] + cmat[1, 2])
     dp[["LR.Positive"]] <- dp[["Sensitivity"]] / (1 - dp[["Specificity"]])
     dp[["LR.Negative"]] <- (1 - dp[["Sensitivity"]]) / dp[["Specificity"]]
     dp[["PV.Positive"]] <- cmat[1, 1] / (cmat[1, 1] + cmat[1, 2])
